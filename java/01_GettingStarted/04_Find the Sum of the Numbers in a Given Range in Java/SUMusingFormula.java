55bc00331a26f458e11c86e1062c0220cbd40269
sohailsj00 <sohailjamadar8793@gmail.com>
2024-09-30 00:31:26 +0530

    day 4

---
+++ java/01_GettingStarted/04_Find the Sum of the Numbers in a Given Range in Java/SUMusingFormula.java
@@ -0,0 +1,16 @@
+
+public class SUMusingFormula {
+
+    public static void main(String[] args) {
+        
+        int a=20;
+        int b=45;
+
+        int sum= b*(b+1)/2-a*(a+1)/2 + a;
+
+        System.out.println(sum);
+
+
+    }
+    
+}
